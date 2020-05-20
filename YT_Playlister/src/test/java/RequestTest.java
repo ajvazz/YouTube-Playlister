75e93eb63c45a71741ee2308ee1101c40c695f29
Nenad Ajvaz <nenadajvaz@hotmail.com>
2020-05-20 11:40:26 +0200

    Project upload #2

---
+++ YT_Playlister/src/test/java/RequestTest.java
@@ -0,0 +1,25 @@
+import io.restassured.response.Response;
+import org.junit.Test;
+
+public class RequestTest {
+
+    private static String validRequest = "https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&regionCode=us&key=AIzaSyDciUo9L8xywPEvPqvMsLfsMIdHjNFo73I&playlistId=PLQ9sFiNxsP2GnltE3MA7-Imx9Mz03HXzl&maxResults=50";
+    private static String invalidRequest = "https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&regionCode=us&key=AIzaSyDciUo9L8xywPEvPqvMsLfsMIdHjNFo73I&playlistId=PLQ9sP2GnltE3MA7-Imx9Mz03HXzl&maxResults=50";
+
+    @Test
+    public void validRequest() {
+        Response response = App.sendRequest(validRequest);
+        response.then().
+                assertThat().
+                statusCode(200);
+    }
+
+    @Test
+    public void invalidRequest() {
+        Response response = App.sendRequest(invalidRequest);
+        response.then().
+                assertThat().
+                statusCode(404);
+    }
+
+}
