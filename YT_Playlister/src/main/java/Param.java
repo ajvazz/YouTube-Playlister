9d1796c9a9ca688c4d8682b1e32d7a837c621d12
Nenad Ajvaz <nenadajvaz@hotmail.com>
2020-06-03 16:01:27 +0200

    Deleted Param class

--- YT_Playlister/src/main/java/Param.java
+++
@@ -1,7 +0,0 @@
-public class Param {
-
-    public static boolean notTesting = true;
-    public static final String newLine = System.lineSeparator();
-    public static final String validPlaylist    = "https://www.youtube.com/playlist?list=PL7DA3D097D6FDBC02";
-    public static final String invalidPlaylist  = "https://www.youtube.com/playlist?list=PL7DA3DD6FDBC02";
-}
